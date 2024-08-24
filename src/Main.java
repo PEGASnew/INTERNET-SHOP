72c73002430aa42cbe154511bc99b62dade2b58b
PEGASnew <nurazizabdumalikov3@gmail.com>
2024-08-24 17:52:03 +0500

    hello sozini ekranga chiqaruvchu kod yozdik

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ package src;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
     }
 }
