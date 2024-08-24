72fdf9a758b7e2e98cbaf5f2cf5467a156ef526b
PEGASnew <nurazizabdumalikov3@gmail.com>
2024-08-24 18:00:27 +0500

    a va b ga son kiritish va chaqirish kodi

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,11 @@ package src;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
+       int a=12345;
+
+       int b=13456;
+
+        System.out.println(a);
+        System.out.println(b);
     }
 }
